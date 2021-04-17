05e0e6ca7039208e1bf546a67d118a6dc3f8f4b0
JumpeiTakubo <jumpeitakubo@nguyens-MacBook-Pro.local>
2021-04-18 05:16:01 +0900

    Context model modified make foreign key nullable

--- mybot/garbage_bot/models.py
+++ mybot/garbage_bot/models.py
@@ -23,6 +23,8 @@ class Area(models.Model):
     district_name = models.CharField(max_length=15, null=True)
     address_name = models.CharField(max_length=64, null=True)
 
+    def __str__(self):
+        return self.town_name +"/"+ self.district_name +"/"+ self.address_name
 class GarbageType(models.Model):
     # TODO: develop the data model.
     # 可燃(週に2回) / 不燃 / 資源 / ペット / 有価物
@@ -66,7 +68,10 @@ class Remind(models.Model):
 class Context(models.Model):
     session_id = models.CharField(max_length=64, null=False)
     state = models.IntegerField() # 各セッション毎に、どこまでユーザとの会話が進んでいるか？
-    garbage_type = models.ForeignKey(GarbageType, on_delete=models.CASCADE)
+    garbage_type = models.ForeignKey(GarbageType, 
+            on_delete=models.CASCADE, 
+            null=True,
+            )
     # area_id = models.ForeignKey(Area, on_delete=models.CASCADE)
     area_candidates = models.JSONField(null=True)
     # 追加したい属性が発生した場合、こちらのカラムで対応する
