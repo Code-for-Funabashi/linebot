580f472a3065c48573beb5e8395ca5a6f2d39377
nyk523jt@gmail.com <takubojunpei@Takubos-MacBook-Air.local>
2021-05-30 01:07:49 +0900

    exec black

--- mybot/urls.py
+++ mybot/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('garbage_bot/', include('garbage_bot.urls')),
+    path("admin/", admin.site.urls),
+    path("garbage_bot/", include("garbage_bot.urls")),
 ]
